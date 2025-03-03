f6adaefde4590677b5b2a0c9c12f352343e47fc1
aviihs <srollins175@gmail.com>
2025-03-03 20:58:24 +0545

    updated featured pages

--- src/components/Home/Featured/Featured.jsx
+++ src/components/Home/Featured/Featured.jsx
@@ -12,61 +12,30 @@ import car6 from "../../../assets/img/car6.png";
 
 const Featured = () => {
   const carsData = [
-    {
-      id: 0,
-      img: car1,
-      name: "Cadillac Escalade",
-      price: "22,440",
-    },
-    {
-      id: 1,
-      img: car2,
-      name: "Cadillac Escalade",
-      price: "22,440",
-    },
-    {
-      id: 2,
-      img: car3,
-      name: "Cadillac Escalade",
-      price: "22,440",
-    },
-    {
-      id: 3,
-      img: car4,
-      name: "Cadillac Escalade",
-      price: "22,440",
-    },
-    {
-      id: 4,
-      img: car5,
-      name: "Cadillac Escalade",
-      price: "22,440",
-    },
-    {
-      id: 5,
-      img: car6,
-      name: "Cadillac Escalade",
-      price: "22,440",
-    },
+    { id: 0, img: car1, name: "Cadillac Escalade", price: "22,440" },
+    { id: 1, img: car2, name: "BMW Series 3", price: "54,890" },
+    { id: 2, img: car3, name: "Mercedes", price: "75,890" },
+    { id: 3, img: car4, name: "BMW 7 Series", price: "55,789" },
+    { id: 4, img: car5, name: "Mercedes Benz", price: "95,776" },
+    { id: 5, img: car6, name: "Range Rover", price: "88,450" },
   ];
 
   const settings = {
     dots: true,
     infinite: true,
-    speed: 2000,
+    speed: 1200, // Smoother transition
     slidesToShow: 3,
     slidesToScroll: 1,
     autoplay: true,
-    autoplaySpeed: 2000,
-    cssEase: "linear",
+    autoplaySpeed: 2000, // More readable
+    cssEase: "ease-in-out",
     arrows: false,
     responsive: [
       {
         breakpoint: 1023,
         settings: {
           slidesToShow: 3,
-          slidesToScroll: 3,
-          infinite: true,
+          slidesToScroll: 1,
           dots: true,
         },
       },
@@ -74,8 +43,7 @@ const Featured = () => {
         breakpoint: 768,
         settings: {
           slidesToShow: 2,
-          slidesToScroll: 2,
-          initialSlide: 2,
+          slidesToScroll: 1,
         },
       },
       {
@@ -83,34 +51,33 @@ const Featured = () => {
         settings: {
           slidesToShow: 1,
           slidesToScroll: 1,
-          initialSlide: 2,
         },
       },
     ],
   };
+
   return (
-    <div>
+    <div className="max-w-full mx-auto py-10">
       <h1 className="font-bold text-center text-4xl">
-        Featured <span className="text-primary ">Cars</span>
+        Featured <span className="text-primary">Cars</span>
       </h1>
-      <p className="">
-        Lorem ipsum dolor sit amet consectetur adipisicing elit. Est laudantium
-        consequuntur rerum mollitia id, nam veniam quae adipisci quos vero nihil
-        aspernatur. Maxime adipisci molestias doloremque possimus, laborum
-        distinctio incidunt!
+      <p className="text-center text-gray-600 max-w-2xl mx-auto mt-3">
+        Explore our exclusive collection of premium cars. Lorem ipsum dolor sit amet
+        consectetur adipisicing elit. Maxime adipisci molestias doloremque possimus.
       </p>
 
-      <div className=""></div>
-      <Slider {...settings}>
-        {carsData.map((item) => (
-          <FeaturedCard
-            key={item.id}
-            img={item.img}
-            name={item.name}
-            price={item.price}
-          />
-        ))}
-      </Slider>
+      <div className="mt-10 ">
+        <Slider {...settings}>
+          {carsData.map((item) => (
+            <FeaturedCard
+              key={item.id}
+              img={item.img}
+              name={item.name}
+              price={item.price}
+            />
+          ))}
+        </Slider>
+      </div>
     </div>
   );
 };
--- src/components/Home/Featured/FeaturedCard.jsx
+++ src/components/Home/Featured/FeaturedCard.jsx
@@ -3,10 +3,10 @@ import React from 'react'
 const FeaturedCard = ({id, img, name, price}) => {
   return (
     <div>
-        <div className="">
+        <div className="border-2 border-secondary text-black bg-slate-100 rounded-xl mb-2 cursor-pointer hover:scale-90 transition duration-200 ease-linear">
             <img src={img} alt="car" />
         </div>
-        <div className="">
+        <div className=" rounded-t-xl w-full">
             <h1>{name}</h1>
             <div className="">
                 <h2>Starting at {price}</h2>
