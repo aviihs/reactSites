39452d1bdb3428973b169a341f3ba1e315f77cd0
aviihs <srollins175@gmail.com>
2025-03-04 17:48:52 +0545

    created whyus.jsx and its folder

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -2,13 +2,15 @@ import React from 'react'
 import Navbar from '../components/Navbar'
 import Hero from '../components/Home/Hero/Hero'
 import Featured from '../components/Home/Featured/Featured'
+import Whyus from '../components/Home/Whyus?/Whyus'
 
 const Home = () => {
   return (
     <>
     < Navbar />
     < Hero />
-    <Featured />
+    < Featured />
+    < Whyus />
     </>
   )
 }
